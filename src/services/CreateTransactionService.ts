2744e6c83e58efed602b5787c9a2e2edce47e23b
Andersonbarros.silva <anderson.tec12@gmail.com>
2020-09-03 05:52:35 -0300

    creater inset new transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,62 @@
 // import AppError from '../errors/AppError';
-
+import { getRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
 
+import Category from '../models/Category';
+
+interface Reqquest {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Reqquest): Promise<any> {
+    const categoryRepository = getRepository(Category);
+    const transactionRepository = getRepository(Transaction);
+
+    const id = async () => {
+      const checkExitsCategory = await categoryRepository.findOne({
+        where: { title: category },
+      });
+
+      let dataCategory: any = '';
+
+      if (!checkExitsCategory) {
+        const newCategory = await categoryRepository.create({
+          title: category,
+        });
+
+        dataCategory = await categoryRepository.save(newCategory);
+      } else {
+        dataCategory = checkExitsCategory;
+      }
+      const { id } = dataCategory;
+
+      return id;
+    };
+
+    const ID = await id();
+
+    const TRANSACTION = async (id: any) => {
+      console.log('########################', id);
+      const newTransaction = transactionRepository.create({
+        type,
+        value,
+        title,
+        category_id: id,
+      });
+
+      const transaction = await transactionRepository.save(newTransaction);
+      return transaction;
+    };
+
+    return TRANSACTION(ID);
   }
 }
 
